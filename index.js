1717e547c79d077c342b62a01063022ef67f61fb
paynejc <paynejc84@gmail.com>
2022-04-24 23:02:46 -0400

    indexjs in main

--- index.js
+++ index.js
@@ -1,54 +1,83 @@
-const inquirer = require('inquirer');
-require('console.table');
-const db = require('./db');
-function start(){
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'menu',
-            message: 'What would you like to do?',
-            choices: [
-                {
-                    name:'view all deparments',
-                    value:'viewDeparments'
-                },
-                {
-                    name: 'view all roles',
-                    value:'viewRoles'
-                },
-                {
-                    name:'quit',
-                    value: 'quit'
-                }
-            ]
-        }
+const inquirer = require("inquirer");
+require("console.table");
+const db = require("./db");
+function start() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menu",
+        message: "What would you like to do?",
+        choices: [
+          {
+            name: "View All Deparments",
+            value: "viewDeparments",
+          },
+          {
+            name: "View All Employees",
+            value: "viewEmployees",
+          },
+          {
+            name: "View All Roles",
+            value: "viewRoles",
+          },
+          {
+            name: "Update Employee",
+            value: "updateEmployee",
+          },
+          {
+            name: "Add Employee",
+            value: "addEmployee",
+          },
+          {
+            name: "Add Role",
+            value: "addRole",
+          },
+          {
+            name: "Quit",
+            value: "quit",
+          },
+        ],
+      },
     ])
-    .then(data => {
-        switch(data.menu){
-            case 'viewDepartments':
-                viewDepartments();
-                break;
-            case 'viewRoles':
-                    viewRoles();
-                    break;
-            default:
-                quit();
-        }
-    })
+    .then((data) => {
+      switch (data.menu) {
+        case "viewDepartments":
+          viewDepartments();
+          break;
+        case "viewEmployees":
+          viewEmployees();
+          break;
+        case "viewRoles":
+          viewRoles();
+          break;
+        case "updateEmployee":
+          updateEmployee();
+          break;
+        case "addEmployee":
+          addEmployee();
+          break;
+        case "addRole":
+          addRole();
+          break;
+        default:
+          quit();
+      }
+    });
 }
 
 function viewDepartments() {
-    db.findDepartments()
-        .then(([departments]) => {
-            console.table(departments)
-        })
-        .then(() => start())
+  db.findDepartments()
+    .then(([departments]) => {
+      console.table(departments);
+    })
+    .then(() => start());
 }
 
-function viewRoles () {}
+function viewRoles() {}
 
-function quit(){
-    process.exit();
+function quit() {
+  process.exit();
 }
 
 start();
